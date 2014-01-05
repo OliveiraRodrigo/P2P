986ccdf86967b444809c05035f5f4f7a0eada31b
Rodrigo <rodrigoufpel@gmail.com>
2014-01-05 03:14:25 -0200

    Rebimento de arquivos binários corrigido. Eu estava usando fgets e fputs em de fgetc e fputc. Dããã.

--- cliente.c
+++ cliente.c
@@ -530,337 +530,64 @@ intptr_t porta(char * ip_destino, intptr_t porta_remota){
                     return porta_destino;
 }
 
-int baixaArquivo(char ip_destino[20]/*, int porta*/, char url[128]){
-    
-    char req[512], shost[64];
-    intptr_t sock; //socket
-    struct sockaddr_in addr;
-    struct in_addr IPV4 = { 0 };
-    //long IPV4;
-    struct hostent * host = NULL;
-    char buffer[10000]; // Buffer para armazenar dados
-    int bytes = 1; // Bytes recebidos
-    int flagAcabouCabecalho = 0;
-    int cont = 0;
-    char *pSplit = NULL;
-    FILE *fp; //arquivo para escrever dados baixados
-    char nomeArquivo[128];
-    
-    pSplit = (char*) malloc(10000*sizeof(char));
-    
-    sprintf(nomeArquivo, "%s.part", url);
-    fp=fopen(nomeArquivo,"wb"); //abra arquivo
-    if(fp==NULL){
-       printf("erro ao criar arquivo");
-       return 1;
-    }
-
-    /*
-    // Cria socket para conectar ao servidor http
-    if((sock = socket(AF_INET,SOCK_STREAM,0))<0){
-        printf("ERRO ao criar socket[1]");
-        return 1;
-    }
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(porta);//seta porta
-    IPV4.s_addr = inet_addr(ip_destino);
-    //inet_aton(ip_destino, &IPV4);
-    host = gethostbyaddr((char *)&IPV4, sizeof(IPV4), AF_INET);
-    
-    if(host == NULL){ //se host não responde, cai fora
-        printf("ERRO ao criar socket [2]");
-        return 1;
-    }
-    
-    //ajusta ip do host
-    memcpy(&addr.sin_addr,host->h_addr_list[0],host->h_length);
-
-    //conecta
-    if(connect(sock,(struct sockaddr*)&addr,sizeof(addr)) < 0){
-        printf(" P2P:> ERRO ao conectar ao servidor para download\n");
-        return 1;
-    }
-    */
-    sock = porta(ip_destino, PORTA_HTTP);
-    
-    // Requisição
-    sprintf(req,"GET /%s HTTP/1.1\r\n\r\n",url);//monta requisição com a url
-    
-    send(sock,req,strlen(req),0); // Envia requisição
-
-
-    while(bytes > 0){ // Enquanto estiver recebendo
-        
-        memset(buffer,0,10000); // Limpa o buffer
-        bytes = recv(sock,buffer,9999,0); // Recebe dados
-        
-        if(bytes > 0){
-            cont++;
-            printf("\nRecebendo parte %d do arquivo...\n", cont);
-        }
-        
-        if(!flagAcabouCabecalho){ //se o cabeçalho ainda não acabou, busca pelo fim dele
-            printf("--aqui--");
-            pSplit = strstr(buffer, "\r\n\r\n"); //se achar o fim do cabeçalho, retorna ponteiro apontando para ele
-            printf("--aqui 0--");
-            pSplit+=4;
-            //printf("\n1\n");
-            printf("--aqui 0.5--");
-        }
-        else{
-            printf("\n2\n");
-            pSplit = NULL;
-        }
-        if(pSplit != NULL) {
-            printf("--aqui 1--");
-            flagAcabouCabecalho=1;//se cabeçalho acabou, seta a flag
-            //printf("%s",pSplit); //imprime parte do buffer que está após o cabeçalho
-            fprintf(fp,"%s",pSplit); //salva parte do buffer que está após o cabeçalho
-            printf("--aqui 2--");
-            printf("\n3\n");
-        }
-        else{
-            printf("--aqui 3--");
-            if(flagAcabouCabecalho==1){ //se cabeçalho já acabou, imprime buffer inteiro
-                printf("--aqui 4--");
-                //printf("%s",buffer);
-                fprintf(fp,"%s",buffer);
-                printf("\n4\n");
-            }
-            printf("--aqui 5--");
-        }
-        printf("--aqui 6--");
-    }
-    printf("--aqui 7--");
-    
-    sprintf(req, "mv %s %s", nomeArquivo, url);//monta string pro system a seguir
-    system(req); //renomeia o arquivo para o nome original (sem ".part" que havia sido adicionado)
-    
-    printf("\nDownload concluído!\n");
-    
-    close(sock); // Fecha socket
-    fclose(fp);
-    
-    return 0;
-}
-
-int httpClient(char ip[20]/*, int porta*/, char url[128]){
-    
-  struct sockaddr_in *remote;
-  struct in_addr IPV4 = { 0 };
-  struct hostent * host = NULL;
-  intptr_t sock;
-  int tmpres;
-  char *get;
-  char buf[BUFSIZ+1];
-  //char *host;
-  FILE *fp; //arquivo para escrever dados baixados
-  char nomeArquivo[128];
-  char req[512];
- 
-  /*//sock = create_tcp_socket();
-  if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
-    perror("Can't create TCP socket");
-    exit(1);
-  }
-  fprintf(stderr, "IP is %s\n", ip);
-  remote = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in *));
-  remote->sin_family = AF_INET;
-  tmpres = inet_pton(AF_INET, ip, (void *)(&(remote->sin_addr.s_addr)));
-  
-  IPV4.s_addr = inet_addr(ip);
-  host = gethostbyaddr((char *)&IPV4, sizeof(IPV4), AF_INET);
-  
-  if( tmpres < 0)  
-  {
-    perror("Can't set remote->sin_addr.s_addr");
-    exit(1);
-  }else if(tmpres == 0)
-  {
-    fprintf(stderr, "%s is not a valid IP address\n", ip);
-    exit(1);
-  }
-  remote->sin_port = htons(porta);
- 
-  if(connect(sock, (struct sockaddr *)remote, sizeof(struct sockaddr)) < 0){
-    perror("Could not connect");
-    exit(1);
-  }*/
-  
-  get = (char*) malloc(10000*sizeof(char));
-  
-  sprintf(nomeArquivo, "%s.part", url);
-    fp=fopen(nomeArquivo,"wb"); //abra arquivo
-    if(fp==NULL){
-       printf("erro ao criar arquivo");
-       return 1;
-    }
-    
-  /*IPV4.s_addr = inet_addr(ip);
-  host = gethostbyaddr((char *)&IPV4, sizeof(IPV4), AF_INET);*/
-  
-  sock = porta(ip, PORTA_HTTP);
-  //sock = porta2(/*ip*/"200.132.39.116", /*PORTA_HTTP*/80);
-  
-  //get = build_get_query(host->h_name, url);
-  //http://www.cs.utah.edu/~swalton/Documents/Articles/Writing-a-C-Based-Client-Server.pdf
-  //http://www.ufsm.br/ccr/revista/boleto.pdf
-  sprintf(get,"GET /%s HTTP/1.1\r\n\r\n",url);//monta requisição com a url
-  fprintf(stderr, "Query is:\n<<START>>\n%s<<END>>\n", get);
- 
-  //Send the query to the server
-  int sent = 0;
-  while(sent < strlen(get))
-  {
-    tmpres = send(sock, get+sent, strlen(get)-sent, 0);
-    if(tmpres == -1){
-      perror("Can't send query");
-      exit(1);
-    }
-    sent += tmpres;
-  }
-  //now it is time to receive the page
-  memset(buf, 0, sizeof(buf));
-  int htmlstart = 0;
-  char * htmlcontent;
-  htmlcontent = (char*) malloc(1024*sizeof(char));
-  int i = 0;
-  printf("-->");
-  while((tmpres = recv(sock, buf, BUFSIZ, 0)) > 0){
-printf("[0]");
-printf("%s", buf);
-      if(!htmlstart){
-//printf("[1]");
-     /* Under certain conditions this will not work.
-      * If the \r\n\r\n part is splitted into two messages
-      * it will fail to detect the beginning of HTML content
-      */
-      htmlcontent = strstr(buf, "\r\n\r\n");
-      if(htmlcontent != NULL){
-//printf("[2]");
-        htmlstart = 1;
-        htmlcontent += 4;
-      }
-    }
-    else{
-//printf("[3]");
-      htmlcontent = buf;
-    }
-    if(htmlstart){
-//printf("[4]");
-      //printf("\nsim\n");
-      fprintf(fp,"%s", htmlcontent);
-        //printf("\n%d ",strlen(htmlcontent));
-        //printf("[%d]\n",sizeof(htmlcontent));
-        //fprintf(stdout, htmlcontent);
-    }
-    else{
-//printf("[5]",i++);
-        //printf("\nnao\n");
-    }
- 
-    //memset(buf, 0, tmpres);
-//printf("[6]",i++);
-  }
-printf("<--");
-  if(tmpres < 0)
-  {
-    perror("Error receiving data");
-  }
-  /*free(get);
-  free(remote);
-  free(ip);*/
-  
-    sprintf(req, "mv %s %s", nomeArquivo, url);//monta string pro system a seguir
-    system(req); //renomeia o arquivo para o nome original (sem ".part" que havia sido adicionado)
-    
-    printf("\nDownload concluído!\n");
-  
-  close(sock);
-  fclose(fp);
-  return 0;
-}
- 
-char *build_get_query(const char *host, char *page)
-{
-  char *query;
-  char *getpage = page;
-  char *tpl = "GET /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n";
-  if(getpage[0] == '/'){
-    getpage = getpage + 1;
-    fprintf(stderr,"Removing leading \"/\", converting %s to %s\n", page, getpage);
-  }
-  // -5 is to consider the %s %s %s in tpl and the ending \0
-  query = (char *)malloc(strlen(host)+strlen(getpage)+strlen(USERAGENT)+strlen(tpl)-5);
-  sprintf(query, tpl, getpage, host, USERAGENT);
-  return query;
-}
-
 int down(char ip[20], char url[128]){
     
     struct hostent* host;
     struct in_addr IPV4 = { 0 };
-    char path[200], req[200], s[BUFSIZ+1], header[BUFSIZ+1], content[2], code[4];
-    FILE *stream, *file;
-    int c, h, i, isHeader;
+    char path[200], req[200], s, header[BUFSIZ+1], code[4];
+    int c, h, isHeader;
     intptr_t sd;
-    sd = porta(ip, PORTA_HTTP);
+    FILE *stream, *file;
     
+    sd = porta(ip, PORTA_HTTP);
     IPV4.s_addr = inet_addr(ip);
     host = gethostbyaddr((char *)&IPV4, sizeof(IPV4), AF_INET);
     //printf("\n{%s}\n", host->h_name);
     
     memset(req, 0, sizeof(req));
-    memset(s, 0, sizeof(s));
     memset(header, 0, sizeof(header));
-    memset(content, 0, sizeof(content));
     memset(code, 0, sizeof(code));
     
-    stream = fdopen(sd, "r+b");         /* convert into stream */
+    stream = fdopen(sd, "r+b"); // Converte em stream
     
     sprintf(path, "downloads/%s", url);
-    file = fopen(path, "wb"); //abra arquivo
+    file = fopen(path, "wb");
     
-    //sprintf(req, "GET /%s HTTP/1.0\r\n\r\n", "/ccr/revista/boleto.pdf"/*url*/);
     sprintf(req, "GET /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n",
-                 /*"/ccr/revista/boleto.pdf"*/url,
+                 url,
                  host->h_name, USERAGENT);
     
-    fprintf(stream, req);      // send request
-    fflush(stream);            // ensure it got out
+    fprintf(stream, req); // Envia a requisicao
+    fflush(stream); // Garante que foi tudo
     
-    //i = 0;
     h = 0;
     isHeader = 1;
-    while ( fgets(s, sizeof(s), stream) != 0 ){  /* while not EOF ...*/
-        i = 0;
-        while(i < strlen(s)){
-            if(isHeader){
-                header[h] = s[i];
-                h++;
-                header[h] = '\0';
-                if(strstr(header, "\r\n\r\n") != NULL){
-                    isHeader = 0;
-                    c = 0;
-                    for(h = 9; h < 12; h++){
-                        code[c] = header[h];
-                        c++;
-                    }
-                    code[c] = '\0';
-                    if(strcmp(code, "200")){
-                        return atoi(code);
-                    }
+    while(!feof(stream)){
+        s = fgetc(stream);
+        if(isHeader){
+            header[h] = s;
+            h++;
+            header[h] = '\0';
+            if(strstr(header, "\r\n\r\n") != NULL){
+                isHeader = 0;
+                c = 0;
+                for(h = 9; h < 12; h++){
+                    code[c] = header[h];
+                    c++;
+                }
+                code[c] = '\0';
+                if(strcmp(code, "200")){
+                    return atoi(code);
                 }
             }
-            else{
-                sprintf(content, "%c", s[i]);
-                fputs(content, file);
+        }
+        else{
+            if(!feof(stream)){
+                fputc(s, file);
             }
-            i++;
         }
     }
     fclose(stream);
     fclose(file);
     return 200;
-//printf("[6]");
 }
